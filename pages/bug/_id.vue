7b886f5338ae48bd8c93828844d22345fea561e5
kelumsampath <edirisingheemks@gmail.com>
2021-05-08 10:29:58 +0530

    column view

--- pages/bug/_id.vue
+++ pages/bug/_id.vue
@@ -1,5 +1,4 @@
 <template>
-  <v-row justify="center" align="center">
     <div
       v-if="bug[0] !== undefined"
       style="
@@ -10,25 +9,31 @@
         background-color: #845460;
       "
     >
-      <div>
-        <p>Bug Id: {{ bug[0].Id }}</p>
-        <h1>{{ bug[0].Summary }}</h1>
-        <h3>{{ bug[0].Description }}</h3>
-        <h3>Story point :{{ bug[0].StroyPoint}}</h3>
-        <h3>Predicted Story Point: {{ bug[0].PredictedStoryPoint }}</h3> 
-        <h3>Developers: </h3>
-        <h4 v-for="dev in bug[0].Name" :key="dev" style="margin-left:10%;">{{dev}}</h4>
+    <p>Bug Id: {{ bug[0].Id }}</p>
+        <h1>{{ bug[0].Summary }}</h1><br/>
+        <v-container >
+      <v-row no-gutters>
+        <v-col cols="12" sm="6" md="8" style="text-align: justify;">
+        <h3>{{ bug[0].Description }}</h3><br/>
         <h3>Comments Story Point: </h3>
         <h4 v-for="cmnt in bug[0].Comment" :key="cmnt" style="margin-left:10%;">{{cmnt}}</h4>
-        <button
+        </v-col>
+        <v-col  cols="6" md="4" style="padding-left:3%">
+          <h3>Developers: </h3>
+        <h4 v-for="dev in bug[0].Name" :key="dev" style="margin-left:10%;">{{dev}}</h4> <br /><br/>
+          <h3>Story point :{{ bug[0].StroyPoint}}</h3>
+          <h3>Predicted Story Point: {{ bug[0].PredictedStoryPoint }}</h3> 
+          <button
               v-on:click="gensp()"
               style="background: green; padding: 1%; border-radius: 8px"
             >
               Genarate Story Point
             </button>
-      </div>
+        </v-col>
+        
+      </v-row>
+        </v-container>
     </div>
-  </v-row>
 </template>
 
 <script>
---
+++ pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <v-row w="12">
+        <v-col w="3">
+          1
+        </v-col>
+        <v-col w="3">
+          2
+        </v-col>
+        
+      </v-row>
+    </div>
+  </template>
